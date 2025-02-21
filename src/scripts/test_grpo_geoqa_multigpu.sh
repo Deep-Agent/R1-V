7f41ef68e7f55d3c52c2439767fd741f95a17a08
kiren <wwkml994@gmail.com>
2025-02-21 07:02:24 +0000

    local

--- src/scripts/test_grpo_geoqa_multigpu.sh
+++ src/scripts/test_grpo_geoqa_multigpu.sh
@@ -1,17 +1,17 @@
 
 
 
-model_path=outputs/checkpoint-100
+model_path=outputs/checkpoint-1000
 batch_size=4
 output_path=./output/train@geo170k/eval/res@checkpoint-30.json
 prompt_path=./src/eval/prompts/geoqa_test_prompts.jsonl
 image_root=./playground
 gpu_ids=0,1,2,3,4,5
 
-python src/eval/test_qwen2vl_geoqa.py \
+python src/eval/test_qwen2vl_geoqa_multigpu.py \
     --model_path ${model_path} \
     --batch_size ${batch_size} \
     --output_path ${output_path} \
     --prompt_path ${prompt_path} \
     --gpu_ids ${gpu_ids} \
-    --image_root
+    --image_root ${image_root}
