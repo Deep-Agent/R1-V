0efba72886bd2aa7565cc5ee9b34c7aa99cb0bcc
Kirenwang <wwkml994@gmail.com>
2025-02-25 23:43:01 +0800

    ocr_r1_v1 vllm

--- src/scripts/run_grpo_qwen25vl_b.sh
+++ src/scripts/run_grpo_qwen25vl_b.sh
@@ -7,11 +7,16 @@ MODEL_PATH=checkpoints/Qwen/VL/Qwen2.5-VL-3B-Instruct
 # MODEL_PATH=checkpoints/Qwen/VL/Qwen2-VL-2B-Instruct
 DATA_PATH=playground/DocVQA-R1
 
+# 256*28*28
+MIN_PIXELS="200704"
+# 1280*28*28
+MAX_PIXELS="1003520"
+
 OUTPUT_DIR=outputs
 export LOG_PATH="./logs/vllm_run_test_7.txt"
 RUN_NAME=ocr1_test_8
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="4" \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="3" \
     --nnodes="1" \
     --node_rank="0" \
     --master_addr="127.0.0.1" \
@@ -20,10 +25,10 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="4" \
     --output_dir $OUTPUT_DIR \
     --model_name_or_path $MODEL_PATH \
     --dataset_name $DATA_PATH \
-    --max_prompt_length 512 \
-    --max_completion_length 1024 \
+    --max_prompt_length 2048 \
+    --max_completion_length 2048 \
     --temperature 1.0 \
-    --num_generations 4 \
+    --num_generations 6 \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 2 \
     --logging_steps 1 \
@@ -31,7 +36,8 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="4" \
     --report_to wandb \
     --gradient_checkpointing true \
     --attn_implementation sdpa \
-    --max_pixels 200000 \
+    --max_pixels $MAX_PIXELS \
+    --min_pixels $MIN_PIXELS \
     --max_steps 13125 \
     --run_name $RUN_NAME \
     --save_steps 1000 \
